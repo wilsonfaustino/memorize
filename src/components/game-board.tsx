d5e51ae197e8b5eb66ff300634880b8e0a623128
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-01 23:12:59 -0300

    feat: implement GameBoard component to display shuffled cards

---
+++ src/components/game-board.tsx
@@ -0,0 +1,18 @@
+import { EMOJIS } from '@/const'
+import { createCardObjects, pickAndShuffleEmojis } from '@/helpers'
+import { Card } from './card'
+
+const CARDS = createCardObjects(pickAndShuffleEmojis(EMOJIS, 8))
+
+export function GameBoard() {
+  return (
+    <div className="grid grid-cols-4 gap-2 rounded-xl bg-blue-100 p-2">
+      {CARDS.map((card) => (
+        <Card
+          key={card.id}
+          {...card}
+        />
+      ))}
+    </div>
+  )
+}
