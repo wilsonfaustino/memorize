4d6b49b48b55af73dd654ec3ceed9fa8c6abfdf3
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-01 23:12:21 -0300

    refactor: replace heading with GameBoard component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { GameBoard } from './components/game-board'
+
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-blue-200">
-      <h1 className="font-bold text-3xl text-pink underline">Hello world!</h1>
+      <GameBoard />
     </div>
   )
 }
