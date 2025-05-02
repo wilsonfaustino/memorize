8c5e53060b44435d73fbe534463e89dd074dd39e
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-02 20:06:22 -0300

    feat: integrate useCards hook and update GameBoard and ScoreBoard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { GameBoard } from './components/game-board'
 import { ScoreBoard } from './components/score-board'
+import { useCards } from './hooks/use-cards'
 
 function App() {
+  const { cards, flipCard, restartGame } = useCards()
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-2 bg-blue-200">
       <ScoreBoard
         moves={0}
-        onRestart={() => {
-          /* Restart logic here */
-        }}
+        onRestart={restartGame}
         time={0}
       />
-      <GameBoard />
+      <GameBoard
+        cards={cards}
+        onCardClick={flipCard}
+      />
     </div>
   )
 }
