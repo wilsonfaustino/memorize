b9e8173f37bf73d7d8ac21e050b8281e24faecac
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-03 16:29:03 -0300

    feat: add DifficultySelector component for selecting game difficulty

---
+++ src/components/difficulty-selector.tsx
@@ -0,0 +1,41 @@
+import * as RadioGroup from '@radix-ui/react-radio-group'
+import { Brain, Sparkles, Zap } from 'lucide-react'
+import { motion } from 'motion/react'
+import { cn } from '@/lib/utils'
+
+const DIFFICULTY_OPTIONS = [
+  { label: 'Easy', value: 'easy', icon: Sparkles, color: 'from-green-500 to-green-700' },
+  { label: 'Medium', value: 'medium', icon: Brain, color: 'from-indigo-500 to-indigo-700' },
+  { label: 'Hard', value: 'hard', icon: Zap, color: 'from-purple-500 to-purple-700' },
+]
+
+export function DifficultySelector() {
+  return (
+    <motion.div
+      animate={{ opacity: 1, y: 0 }}
+      className="flex w-full flex-col gap-8 px-4"
+      exit={{ opacity: 0 }}
+      initial={{ opacity: 0, y: -300 }}>
+      <h2 className="text-center font-bold text-2xl text-white sm:text-3xl">Select Difficulty</h2>
+      <RadioGroup.Root
+        className="flex flex-col justify-center gap-3 sm:flex-row sm:gap-4 "
+        onValueChange={(value) => console.log(value)}>
+        {DIFFICULTY_OPTIONS.map(({ label, value, icon: Icon, color }) => (
+          <RadioGroup.Item
+            asChild
+            key={value}
+            value={value}>
+            <button
+              className={cn('h-20 w-full rounded-xl bg-gradient-to-b sm:size-32', color)}
+              type="button">
+              <div className="flex items-center justify-center gap-2 sm:flex-col">
+                <Icon className="size-6 sm:size-8" />
+                <span className="font-medium text-base sm:text-lg">{label}</span>
+              </div>
+            </button>
+          </RadioGroup.Item>
+        ))}
+      </RadioGroup.Root>
+    </motion.div>
+  )
+}
