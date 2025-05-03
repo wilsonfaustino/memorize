cefc76793d66c7f2cb68afc395f888a7cf6eb83b
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-03 19:00:20 -0300

    refactor: remove unused imports and memoization for matched cards in useCards hook

--- src/hooks/use-cards.ts
+++ src/hooks/use-cards.ts
@@ -1,7 +1,6 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import type { MemoryCard } from '@/@types'
-import { EMOJIS, PairCount } from '@/const'
-import { createCardObjects, pickAndShuffleEmojis, setupEmojiArray, shuffleArray, sleep } from '@/helpers'
+import { setupEmojiArray, shuffleArray, sleep } from '@/helpers'
 import { useCounter } from '@/hooks/use-counter'
 import { useDisclosure } from '@/hooks/use-disclosure'
 import { useTimer } from '@/hooks/use-timer'
@@ -17,12 +16,6 @@ export function useCards({ difficulty }: UseCardsProps) {
   const { counter, incrementCounter, resetCounter } = useCounter({ initialValue: 0 })
   const { time, startTimer, resetTimer, stopTimer, isRunning } = useTimer()
   const endTriggered = useRef(false)
-
-  const isAllCardsMatched = useMemo(() => {
-    const allCardsMatched = cards.length > 0 && cards.every((card) => card.isMatched)
-
-    return allCardsMatched
-  }, [cards])
 
   const checkForMatch = useCallback(
     (firstId: string, secondId: string) => {
