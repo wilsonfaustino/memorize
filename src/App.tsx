d6529a2173e87aeaf63a827f82b2d1cc6a489f63
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-03 16:28:58 -0300

    feat: refactor App component to use AppContainer for layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
+import { AppContainer } from '@/components/app-container'
+import { Card } from '@/components/card'
+import { DifficultySelector } from '@/components/difficulty-selector'
 import { EndGameModal } from '@/components/end-game-modal'
 import { GameBoard } from '@/components/game-board'
 import { ScoreBoard } from '@/components/score-board'
 import { formatTime } from '@/helpers'
 import { useCards } from '@/hooks/use-cards'
-import { Card } from './components/card'
 
 function App() {
+  const difficulty = undefined
   const { cards, flipCard, restartGame, isOpen, moves, time, checkCardTemporaryFlipped } = useCards()
   const formattedTime = formatTime(time)
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-2 bg-blue-200">
-      <ScoreBoard
-        moves={moves}
-        onRestart={restartGame}
-        time={formattedTime}
-      />
-      <GameBoard>
-        {cards.map(({ id, emoji, isFlipped, isMatched }) => (
-          <Card
-            emoji={emoji}
-            isFlipped={isFlipped || checkCardTemporaryFlipped(id)}
-            isMatched={isMatched}
-            key={id}
-            onClick={() => flipCard(id)}
+    <AppContainer>
+      {!difficulty ? (
+        <DifficultySelector />
+      ) : (
+        <>
+          <ScoreBoard
+            moves={moves}
+            onRestart={restartGame}
+            time={formattedTime}
           />
-        ))}
-      </GameBoard>
-      <EndGameModal
-        isOpen={isOpen}
-        moves={moves}
-        onRestart={restartGame}
-        time={formattedTime}
-      />
-    </div>
+          <GameBoard>
+            {cards.map(({ id, emoji, isFlipped, isMatched }) => (
+              <Card
+                emoji={emoji}
+                isFlipped={isFlipped || checkCardTemporaryFlipped(id)}
+                isMatched={isMatched}
+                key={id}
+                onClick={() => flipCard(id)}
+              />
+            ))}
+          </GameBoard>
+          <EndGameModal
+            isOpen={isOpen}
+            moves={moves}
+            onRestart={restartGame}
+            time={formattedTime}
+          />
+        </>
+      )}
+    </AppContainer>
   )
 }
 
