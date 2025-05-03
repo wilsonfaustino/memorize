b74b2559be5984d6475364f44d6c2430a535f96a
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-03 14:12:09 -0300

    feat: enhance useCards hook with improved match checking and game restart logic

--- src/hooks/use-cards.ts
+++ src/hooks/use-cards.ts
@@ -1,6 +1,10 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import type { MemoryCard } from '@/@types'
 import { EMOJIS } from '@/const'
 import { createCardObjects, pickAndShuffleEmojis, shuffleArray, sleep } from '@/helpers'
+import { useCounter } from '@/hooks/use-counter'
+import { useDisclosure } from '@/hooks/use-disclosure'
+import { useTimer } from '@/hooks/use-timer'
 
 export function useCards() {
   const [cards, setCards] = useState(() => {
@@ -8,36 +12,78 @@ export function useCards() {
     return createCardObjects(emojis)
   })
   const [flippedCards, setFlippedCards] = useState<string[]>([])
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { counter, incrementCounter, resetCounter } = useCounter({ initialValue: 0 })
+  const { time, startTimer, resetTimer, stopTimer, isRunning } = useTimer()
+  const endTriggered = useRef(false)
 
-  const checkForMatch = (id: string) => {
-    if (!flippedCards.length) {
-      setFlippedCards([id])
-      return
-    }
+  const isAllCardsMatched = useMemo(() => {
+    const allCardsMatched = cards.every((card) => card.isMatched)
 
-    const [firstCardId] = flippedCards
-    const firstCard = cards.find((card) => card.id === firstCardId)
-    const secondCard = cards.find((card) => card.id === id)
-    if (!(firstCard && secondCard && firstCard.emoji === secondCard.emoji)) {
-      setTimeout(() => {
-        setCards((prevCards) =>
-          prevCards.map((card) => (card.id === firstCardId || card.id === id ? { ...card, isFlipped: false } : card)),
-        )
-        setFlippedCards([])
-      }, 1000)
-      return
-    }
+    return allCardsMatched
+  }, [cards])
 
-    setCards((prevCards) =>
-      prevCards.map((card) => (card.id === firstCardId || card.id === id ? { ...card, isMatched: true } : card)),
-    )
-    setFlippedCards([])
-  }
+  const checkForMatch = useCallback(
+    (firstId: string, secondId: string) => {
+      const firstCard = cards.find((card) => card.id === firstId)
+      const secondCard = cards.find((card) => card.id === secondId)
+      if (!(firstCard && secondCard && firstCard.emoji === secondCard.emoji)) {
+        incrementCounter()
+        setTimeout(() => {
+          setCards((prevCards) =>
+            prevCards.map((card) =>
+              card.id === firstId || card.id === secondId ? { ...card, isFlipped: false } : card,
+            ),
+          )
+          setFlippedCards([])
+        }, 1000)
+        return
+      }
 
-  const flipCard = (id: string) => {
-    setCards((prevCards) => prevCards.map((card) => (card.id === id ? { ...card, isFlipped: !card.isFlipped } : card)))
-    checkForMatch(id)
-  }
+      setCards((prevCards) =>
+        prevCards.map((card) => (card.id === firstId || card.id === secondId ? { ...card, isMatched: true } : card)),
+      )
+      setFlippedCards([])
+      incrementCounter()
+    },
+    [cards, incrementCounter],
+  )
+
+  const flipCard = useCallback(
+    (id: string) => {
+      if (!isRunning) {
+        startTimer()
+      }
+
+      if (!flippedCards.length) {
+        setFlippedCards([id])
+        setCards((prevCards) => prevCards.map((card) => (card.id === id ? { ...card, isFlipped: true } : card)))
+        return
+      }
+
+      const newFlippedCards = [...flippedCards, id]
+
+      const [firstCardId] = flippedCards
+
+      if (id === firstCardId || newFlippedCards.length > 2) return
+
+      setFlippedCards(newFlippedCards)
+      setCards((prevCards) => prevCards.map((card) => (card.id === id ? { ...card, isFlipped: true } : card)))
+      checkForMatch(firstCardId, id)
+    },
+    [checkForMatch, isRunning, startTimer, flippedCards],
+  )
+
+  const performRestartSequence = useCallback(
+    (newCards: MemoryCard[]) => {
+      setCards(newCards)
+      isOpen && onClose()
+      resetCounter()
+      resetTimer()
+      endTriggered.current = false
+    },
+    [isOpen, onClose, resetCounter, resetTimer],
+  )
 
   const restartGame = async () => {
     const newCards = cards.map((card) => ({ ...card, isFlipped: false, isMatched: false }))
@@ -45,8 +91,26 @@ export function useCards() {
     setFlippedCards([])
     const shuffledCards = shuffleArray([...newCards])
     await sleep(1000)
-    setCards(shuffledCards)
+
+    performRestartSequence(shuffledCards)
   }
 
-  return { cards, flipCard, restartGame }
+  const checkCardTemporaryFlipped = useCallback(
+    (id: string) => {
+      return flippedCards.includes(id)
+    },
+    [flippedCards],
+  )
+
+  useEffect(() => {
+    if (isAllCardsMatched && !endTriggered.current) {
+      endTriggered.current = true
+      onOpen()
+      stopTimer()
+    }
+  }, [isAllCardsMatched, onOpen, stopTimer])
+
+  console.log('flippedCards', flippedCards)
+
+  return { cards, flipCard, restartGame, isOpen, moves: counter, time, checkCardTemporaryFlipped }
 }
