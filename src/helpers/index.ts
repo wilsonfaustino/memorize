e19ead4f4e2c970a60218abfb80eff071afe502f
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-03 14:12:03 -0300

    feat: add formatTime function to convert seconds into a formatted string

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -36,3 +36,9 @@ export const shuffleArray = <T>(array: T[]): T[] => {
 export async function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
+
+export function formatTime(seconds: number): string {
+  const minutes = Math.floor(seconds / 60)
+  const remainingSeconds = seconds % 60
+  return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`
+}
