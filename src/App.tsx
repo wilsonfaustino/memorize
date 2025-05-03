7a400df019c31c3d4280905d5f10e0b0a51dfd77
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-03 14:11:21 -0300

    feat: enhance App component to manage game state and display dynamic scoreboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { GameBoard } from './components/game-board'
-import { ScoreBoard } from './components/score-board'
-import { useCards } from './hooks/use-cards'
+import { EndGameModal } from '@/components/end-game-modal'
+import { GameBoard } from '@/components/game-board'
+import { ScoreBoard } from '@/components/score-board'
+import { formatTime } from '@/helpers'
+import { useCards } from '@/hooks/use-cards'
+import { Card } from './components/card'
 
 function App() {
-  const { cards, flipCard, restartGame } = useCards()
+  const { cards, flipCard, restartGame, isOpen, moves, time, checkCardTemporaryFlipped } = useCards()
+  const formattedTime = formatTime(time)
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-2 bg-blue-200">
       <ScoreBoard
-        moves={0}
+        moves={moves}
         onRestart={restartGame}
-        time={0}
+        time={formattedTime}
       />
-      <GameBoard
-        cards={cards}
-        onCardClick={flipCard}
+      <GameBoard>
+        {cards.map(({ id, emoji, isFlipped, isMatched }) => (
+          <Card
+            emoji={emoji}
+            isFlipped={isFlipped || checkCardTemporaryFlipped(id)}
+            isMatched={isMatched}
+            key={id}
+            onClick={() => flipCard(id)}
+          />
+        ))}
+      </GameBoard>
+      <EndGameModal
+        isOpen={isOpen}
+        moves={moves}
+        onRestart={restartGame}
+        time={formattedTime}
       />
     </div>
   )
