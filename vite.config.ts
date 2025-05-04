ddf804718a6f6a418d014228a8dae7a208b2ae31
Wilson Faustino <wilson.faustino@syngenta.com>
2025-05-04 12:01:08 -0300

    feat: add testing setup with jest-dom and configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+/// <reference types="vitest/config" />
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +11,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    globals: true,
+  }
 })
